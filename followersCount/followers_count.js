a84ccdc3b690c8880fb73c008ce36181dad15e38
Mark-Cyber0628 <szakalmark0628@gmail.com>
2025-06-14 10:44:19 -0400

    reset button test

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -1,23 +1,24 @@
 let count = 0; // Initialize count to 0
+
 function increaseCount() {
-    count++; // Növeld a count-ot 1-el
-}
-function displayCount() {
-    document.getElementById('countDisplay').innerHTML=count; // Megjeleníti a számot a HTML-ben.
-}
-function increaseCount () {
-    count++;
+    count++; // Increase count by 1
     displayCount();
     checkCountValue();
 }
-function checkCountValue() {
-    if (count === 10) {
-      alert("Az Instagram posztod 10 követőt szerzett! Gratulálunk!");
-    } else if (count === 20) {
-      alert("Az Instagram posztod 20 követőt szerzett! Csak így tovább!");
-    }
-  }
-  function resetCount() {
+
+function resetCount() {
     count = 0;
     document.getElementById('countDisplay').textContent = count;
-  }
+}
+
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML = count; // Display the count in HTML
+}
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("Az Instagram posztod 10 követőt szerzett! Gratulálunk!");
+    } else if (count === 20) {
+        alert("Az Instagram posztod 20 követőt szerzett! Csak így tovább!");
+    }
+}
